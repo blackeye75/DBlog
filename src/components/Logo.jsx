43a06ccfc71d6de0e64534385bc75e87423a7031
blackeye75 <priyanshuraj22275@gmail.com>
2024-06-24 01:55:45 +0530

    UI fix minor

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Logo = ({ width = "100px" }) => {
-  return <div className="font-bold text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" >DBlog</div>;
+  return <div className="font-bold  text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]" >DBlog</div>;
 };
 
 export default Logo;
