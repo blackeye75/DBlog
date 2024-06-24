cd2c71aabdb60ad25b9c15a8e768c340870c4dd2
blackeye75 <priyanshuraj22275@gmail.com>
2024-06-24 20:28:53 +0530

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import "./App.css";
 import { useState,useEffect } from "react";
 import authService from "./appwrite/auth";
